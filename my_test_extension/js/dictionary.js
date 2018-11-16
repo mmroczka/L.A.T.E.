207d06fce0407b9ed527186c6936aa9f7b76d6b2
Michael Mroczka <michaelmroczka@gmail.com>
2018-11-15 23:31:36 -0500

    Add ability to store settings locally on Dictionary Mode page

--- my_test_extension/js/dictionary.js
+++ my_test_extension/js/dictionary.js
@@ -7,14 +7,41 @@ $(document).ready(function() {
     } );
 } );
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     var link = document.getElementById("click-this");
-//     link.addEventListener("click", function() {
-//         hellYeah('xxx');
-//     });
-// });
 
-// function hellYeah(text) {
-//   document.getElementById("text-holder").innerHTML = text;
-// }
+function saveChanges() {
+    // Save it using the Chrome extension storage API.
+    var dictionaryMode = $('#dictionary_id').is(":checked");
+    chrome.storage.sync.set({'dictionaryMode': dictionaryMode});
+}
+
+function rememberPreviousSettings(){
+    chrome.storage.sync.get(null, function(storage) {
+        // alert(JSON.stringify(storage, null, 4));
+        if (storage.dictionaryMode === undefined){
+            alert("Undefined");
+        } 
+        else if (storage.dictionaryMode === true){
+            $('#dictionary_id').prop("checked", storage.dictionaryMode)
+        } else{
+            $('#dictionary_id').prop("checked", storage.dictionaryMode)
+        }
+
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var dictionaryModeButton = document.getElementById("dictionary_id");
+    rememberPreviousSettings();
+    dictionaryModeButton.addEventListener("click", function() {
+        saveChanges();
+    });
+});
+
+
+// chrome.storage.onChanged.addListener(function(changes, namespace) {
+//     for (key in changes) {
+//         var storageChange = changes[key];
+//         // alert('Storage key ' + key + ' in namespace ' + namespace + ' changed. Old value was ' + storageChange.oldValue + ', new value is ' + storageChange.newValue);
+//     }
+// });
 
