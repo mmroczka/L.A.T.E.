73f55ab65d8086865aeb5354e7c8199ec87d08f6
Michael Mroczka <michael.mroczka@gmail.com>
2018-11-16 17:25:10 -0500

    Still working on saving random modes settings

--- my_test_extension/js/random.js
+++ my_test_extension/js/random.js
@@ -7,22 +7,40 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("random_id").addEventListener("click", function() {
         saveChanges();
     });
+
+    // Add languageFrom auto-save
+    document.getElementById("languageFrom").addEventListener("change", function() {
+        saveChanges();
+    });
+
+    // Add languageTo auto-save
+    document.getElementById("languageTo").addEventListener("change", function() {
+        saveChanges();
+    });
 });
 
 function saveChanges() {
     // Save it using the Chrome extension storage API.
-    var randomMode = $('#random_id').is(":checked");
-    chrome.storage.sync.set({'randomMode': randomMode});
+    var v = $('#languageFrom option:selected').val();
+    alert(JSON.stringify(v, null, 4));
+    chrome.storage.sync.set({'randomMode': $('#random_id').is(":checked")});
+    chrome.storage.sync.set({'languageFrom': $('#languageFrom option:selected').val()});
+    chrome.storage.sync.set({'languageTo': $('#languageTo option:selected').val()});
 }
 
 function rememberPreviousSettings(){
     chrome.storage.sync.get(null, function(storage) {
-        // alert(JSON.stringify(storage, null, 4));
-        if (storage.randomMode === undefined){
+        alert(JSON.stringify(storage, null, 4));
+        if (storage.randomMode === undefined || storage.languageFrom === undefined || storage.languageTo === undefined){
+            alert("something undefined");
             chrome.storage.sync.set({'randomMode': false});
+            chrome.storage.sync.set({'languageFrom': "Detect Language"});
+            chrome.storage.sync.set({'languageTo': "French"});
         }
         else{
             $('#random_id').prop("checked", storage.randomMode)
+            $('#languageFrom').value = storage.languageFrom;
+            $('#languageTo').value = storage.languageTo;
         }
 
     });
