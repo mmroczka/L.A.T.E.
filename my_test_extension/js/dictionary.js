8ef46868a34aaf5497ee2303f617374f3771e3cd
Michael Mroczka <michaelmroczka@gmail.com>
2018-11-18 20:38:44 -0500

    Change DataTable object to pull from objects instead of txt file

--- my_test_extension/js/dictionary.js
+++ my_test_extension/js/dictionary.js
@@ -1,11 +1,56 @@
 $(document).ready(function() {
-    $('#example').DataTable( {
-        "ajax": "data/dummy_dictionary.txt",
+    var table = $('#example').DataTable( {
+        columns : [
+            {data: null, render: 'word'},
+            {data: null, render: 'translation'},
+            {data: null, render: 'source'},
+            {data: null, render: 'skipWord'}
+        ],
         "scrollY": "200px",
         "scrollCollapse": true,
         "paging": false
-    } );
+    });
+
+    table.row.add(new Word("the", "et", "Duolingo", false));
+    table.row.add(new Word("variety", "variété", "Duolingo", false));
+    table.row.add(new Word("computer", "ordinateur", "Upload", true));
+    table.draw();
 } );
+
+function getDictionary(){
+    chrome.storage.sync.get(null, function(storage) {
+        if (storage.dictionary === undefined){
+            return JSON.parse(storage.dictionary);
+        } else{
+            return JSON.parse(storage.dictionary);
+        }
+    });
+};
+
+function getDictionaryFromFile(){
+};
+
+
+
+function Word (word, translation, source, skipWord){
+    this._word = word;
+    this._translation = translation;
+    this._source = source;
+    this._skipWord = skipWord;
+
+    this.word = function (){
+        return this._word;
+    }
+    this.translation = function (){
+        return this._translation;
+    }
+    this.source = function (){
+        return this._source;
+    }
+    this.skipWord = function (){
+        return this._skipWord;
+    }
+};
 
 // When page is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
--- my_test_extension/js/translate.js
+++ my_test_extension/js/translate.js
@@ -6,15 +6,41 @@ function translate() {
     chrome.storage.sync.get(null, function(storage){
         if (storage.dictionaryMode != undefined && storage.dictionaryMode == true){
             dictionaryModeSwap();
+            // buildDictionary();
         }
     });
 }
 
 function dictionaryModeSwap(){
+    $.each($("p"), function(index, paragraph) {
+        var $paragraph = $(paragraph);
+        var pString = $paragraph.text();
+        pString = pString.split(" ");
+        var wordList = [];
+        // assuming the pString isn't empty...
+        if (pString[0] != "") {
+            for (var wordIndex = 0; wordIndex < pString.length; wordIndex++){
+                var word = pString[wordIndex];
+                word = word.toLowerCase();
+                // wordList.push(word);
+                if (word in dictionary.data){
+                    alert("The word (" + word + ") is in the dictionary!")
+                } else{
+                    alert("The word (" + word + ") is NOT in the dictionary!")
+                }
+            }
+        }
+    });
+}
+
+function buildDictionary(){
     dictAsString = ' { "data": [ [ "and", "et", "Duolingo", "false" ], [ "variety", "variété", "Duolingo", "false" ], [ "computer", "ordinateur", "Uploaded", "true" ], [ "and", "et", "Duolingo", "false" ], [ "variety", "variété", "Duolingo", "false" ], [ "computer", "ordinateur", "Uploaded", "true" ], [ "and", "et", "Duolingo", "false" ] ] }';
-    alert("in dictionaryModeSwap");
-    var pTags = $("p").text();
-    alert(JSON.stringify(pTags, null, 4));
-    var pTagz = $("p").html();
-    alert(JSON.stringify(pTagz, null, 4));
+
+    dictionaryJson = JSON.parse(dictAsString);
+    alert(JSON.stringify(dictionaryJson, null, 4));
+    dictionary = {};
+    for (var i = 0; i < dictionaryJson.length; i++){
+        // var word = dictionaryJson[i].
+    }
+
 }
