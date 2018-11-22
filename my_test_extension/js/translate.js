d58aadf30c71acf9fcbf8c73553695017b1ba323
Michael Mroczka <michaelmroczka@gmail.com>
2018-11-22 11:54:21 -0500

    Add regex and conditional logic to fix punction/spacing problems

--- my_test_extension/js/translate.js
+++ my_test_extension/js/translate.js
@@ -14,67 +14,35 @@ function translate() {
                 if (pText != ""){
                     Object.keys(dictionary).forEach(function(key, index){
                         if (!dictionary[key].skipWord){
-                            // if (index == 0){
-                            //     // When at beginning of sentence, we don't need to add a space in the front
-                            //     htmlString = htmlString.replace(key + " ", '<element style="color:red; id="translation">' +  dictionary[key].translation + " " + "</element>");
-                            // } else if (index == pText.length) {
-                            //     // When at end of sentence, we don't need to add a space at the back
-                            //     htmlString = htmlString.replace(" " + key, '<element style="color:red; id="translation">' + " " +  dictionary[key].translation + "</element>");
-                            // } else{
-                            //     // When in the middle of a sentence, we need to add a space at the front and back
-                            //     htmlString = htmlString.replace(" " + key + " ", '<element style="color:red; id="translation">' + " " + dictionary[key].translation + " " + "</element>");
-                            // }
-                            // htmlString = htmlString.replace(key + " ", '<element style="color:red; id="translation">' +  dictionary[key].translation + "</element>");
-                            var translatedWord = '<element style="color:red; id="translation">' +  dictionary[key].translation + "</element>"
-                            var regex = new RegExp("[>]([^<]*)(" + key + ")([^<]*)", "gi");
-                            // alert(JSON.stringify(regex, null, 2));
+                            var regex = new RegExp("[>]([^<]*)(?![^a-z|A-Z])(" + key + ")(?=[^a-r|A-R|t-z|T-Z])([^<]*)", "gi");
                             let paragraphMatches = getMatches(htmlString, regex);
                             if (paragraphMatches.length > 0){
                                 for (var i = 0; i < paragraphMatches.length; i++){
-                                    // alert(JSON.stringify(paragraphMatches[i], null, 2));
+                                    var translatedWord = dictionary[key].translation;
                                     var match = paragraphMatches[i];
-                                    var fullTextToSwap = ">" + match[1] + translatedWord + match[3];
-                                    // alert("Swapping " + match[0] + "\n\nwith " + fullTextToSwap);
+                                    var beginningElement = '<element style="color:red; id="translation">' + ' ';
+                                    var endingElement = ' ' + '</element>';
+                                    if (match[2].charAt(0) == key.charAt(0).toUpperCase()){
+                                        // word we are translating is uppercase, so change key to uppercase to match it
+                                        translatedWord = translatedWord.charAt(0).toUpperCase() + translatedWord.slice(1);
+                                    }
+                                    if (/[^a-z|A-Z]/i.test(match[1].substr(-1))){
+                                        // if there is something non-alphanumeric right before our keyword it's probably a hyphenated word or a punction of some type, so don't add a space
+                                        beginningElement = '<element style="color:red; id="translation">';
+                                    }
+                                    if (match[0].toLowerCase().includes(key+'s')){
+                                        // word is most likely plural so don't add space at the end to keep the original word's "s"
+                                        endingElement = "</element>";
+                                    }
+
+                                    var fullTextToSwap = ">" + match[1] + beginningElement + translatedWord + endingElement + match[3];
                                     htmlString = htmlString.replace(match[0], fullTextToSwap);
                                 }
                             }
-                            // var match;
-                            // for (match in paragraphMatches){
-                            //     if (match != 0){
-                            //         alert(JSON.stringify(match[1], null, 2));
-                            //     }
-                            //     htmlString = htmlString.replace(key + " ", '<element style="color:red; id="translation">' +  dictionary[key].translation + "</element>");
-                            // }
                         }
                     });
 
                     paragraph.html(htmlString);
-
-                    // alternate way of replacing using regex and avoiding html tags
-                    /*
-                     *  var regex = /[>]([^<]*)(computer)([^<]*)/g
-                     *  var translateWord = "ordinateur";
-                     *   let result;
-                     *   while(result = regex.exec(str)){
-                     *     str = str.replace(result[1], ">" + translateWord);
-                     *   }
-                     *   console.log(str);
-                     *
-                     * */
-                    // var pString = $paragraph.text();
-                    // pString = pString.split(" ");
-                    // // assuming the pString isn't empty...
-                    // if (pString[0] != "") {
-                    //     for (var wordIndex = 0; wordIndex < pString.length; wordIndex++){
-                    //         var word = pString[wordIndex];
-                    //         word = word.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").toLowerCase();
-                    //         if (word.length > 0){
-                    //             if (word in dictionary){
-                    //                 alert("The word (" + word + ") translates to " + dictionary[word].translation);
-                    //             }
-                    //         }
-                    //     }
-                    // }
                 }
             });
         }
