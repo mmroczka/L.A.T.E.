5985beee1a610928ed02fced5b958e108e19604b
Michael Mroczka <michaelmroczka@gmail.com>
2018-11-23 19:18:02 -0500

    Add Tippy.js library and basic Tippy working for words

--- my_test_extension/js/translate.js
+++ my_test_extension/js/translate.js
@@ -6,9 +6,11 @@ function translate() {
     chrome.storage.sync.get(null, function(storage){
         if (storage.dictionaryMode != undefined && storage.dictionaryMode == true){
             dictionary = JSON.parse(storage.dictionary);
+            var changeWordColor = "";
             if (storage.changeWordColor == true){
-                var changeWordColor = 'style="color:' + storage.wordColor + ';';
+                changeWordColor = 'style="color:' + storage.wordColor + '";';
             }
+            var dictionaryTranslationsCount = 0;
             $.each($("p"), function(index, paragraph) {
                 var paragraph = $(paragraph);
                 var pText = paragraph.text();
@@ -23,7 +25,14 @@ function translate() {
                                 for (var i = 0; i < paragraphMatches.length; i++){
                                     var translatedWord = dictionary[key].translation;
                                     var match = paragraphMatches[i];
-                                    var beginningElement = '<element ' + changeWordColor + ' id="translation">' + ' ';
+                                    var tooltip = "";
+                                    if (storage.tooltip){
+                                        // tooltip = 'data-tippy="' + match[2] + '" data-tippy-delay="500"';
+                                        tippy('', {
+
+                                        });
+                                    }
+                                    var beginningElement = '<element ' + changeWordColor + ' id="translation' + dictionaryTranslationsCount + '" ' + tooltip + '>' + ' ';
                                     var endingElement = ' ' + '</element>';
                                     if (match[2].charAt(0) == key.charAt(0).toUpperCase()){
                                         // word we are translating is uppercase, so change key to uppercase to match it
@@ -31,7 +40,7 @@ function translate() {
                                     }
                                     if (/[^a-z|A-Z]/i.test(match[1].substr(-1))){
                                         // if there is something non-alphanumeric right before our keyword it's probably a hyphenated word or a punction of some type, so don't add a space
-                                        beginningElement = '<element ' + changeWordColor + ' id="translation">';
+                                        beginningElement = '<element ' + changeWordColor + ' id="translation' + dictionaryTranslationsCount + '" ' + tooltip + '>';
                                     }
                                     if (match[0].toLowerCase().includes(key+'s')){
                                         // word is most likely plural so don't add space at the end to keep the original word's "s"
@@ -40,6 +49,13 @@ function translate() {
 
                                     var fullTextToSwap = ">" + match[1] + beginningElement + translatedWord + endingElement + match[3];
                                     htmlString = htmlString.replace(match[0], fullTextToSwap);
+                                    var $translation = $("#translation" + dictionaryTranslationsCount);
+                                    tippy.setDefaults({
+                                        arrow: true,
+                                        delay: 40,
+                                        theme: 'my-tippy'
+                                    });
+                                    dictionaryTranslationsCount += 1;
                                 }
                             }
                         }
