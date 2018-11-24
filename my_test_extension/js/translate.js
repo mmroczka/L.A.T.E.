eebf76b427146b2f53ba382969e80c2c03c69ded
Michael Mroczka <michaelmroczka@gmail.com>
2018-11-24 12:44:18 -0500

    Switch from grabbing all p tags to just grabbing the entire body element

--- my_test_extension/js/translate.js
+++ my_test_extension/js/translate.js
@@ -11,47 +11,46 @@ function translate() {
                 changeWordColor = 'style="color:' + storage.wordColor + '";';
             }
             var dictionaryTranslationsCount = 0;
-            $.each($("p"), function(index, paragraph) {
-                var paragraph = $(paragraph);
-                htmlString = "<p>" + paragraph.html() + "</p>";
-                Object.keys(dictionary).forEach(function(key, index){
-                    if (!dictionary[key].skipWord){
-                        let paragraphMatches = [];
-                        do {
-                            var regex = new RegExp("[>]([^<]*)(?![^a-z|A-Z])(" + key + ")([^<]*)", "gi");
-                            paragraphMatches = getMatches(htmlString, regex);
-                            if (paragraphMatches.length > 0){
-                                for (var i = 0; i < paragraphMatches.length; i++){
-                                    var translatedWord = dictionary[key].translation;
-                                    var match = paragraphMatches[i];
-                                    var tooltip = "";
-                                    if (storage.tooltip){
-                                        tooltip = 'data-tippy="' + match[2] + '" data-tippy-delay="500" data-tippy-animation="perspective"';
-                                    }
-                                    var beginningElement = '<element ' + changeWordColor + ' id="translation' + dictionaryTranslationsCount + '" ' + tooltip + '>' + ' ';
-                                    var endingElement = ' ' + '</element>';
-                                    if (match[2].charAt(0) == key.charAt(0).toUpperCase()){
-                                        // word we are translating is uppercase, so change key to uppercase to match it
-                                        translatedWord = translatedWord.charAt(0).toUpperCase() + translatedWord.slice(1);
-                                    }
-                                    if (/[^a-z|A-Z]/i.test(match[1].substr(-1))){
-                                        // if there is something non-alphanumeric right before our keyword it's probably a hyphenated word or a punction of some type, so don't add a space
-                                        beginningElement = '<element ' + changeWordColor + ' id="translation' + dictionaryTranslationsCount + '" ' + tooltip + '>';
-                                    }
-                                    if (match[0].toLowerCase().includes(key+'s')){
-                                        // word is most likely plural so don't add space at the end to keep the original word's "s"
-                                        endingElement = "</element>";
-                                    }
-                                    var fullTextToSwap = ">" + match[1] + beginningElement + translatedWord + endingElement + match[3];
-                                    htmlString = htmlString.replace(match[0], fullTextToSwap);
-                                    dictionaryTranslationsCount += 1;
+
+            var bodyElement = document.getElementsByTagName("BODY")[0];
+            var bodyHTML = bodyElement.outerHTML;
+            Object.keys(dictionary).forEach(function(key, index){
+                if (!dictionary[key].skipWord){
+                    let paragraphMatches = [];
+                    do {
+                        var regex = new RegExp("[>]([^<]*)(?![^a-z|A-Z])(" + key + ")([^<]*)", "gi");
+                        paragraphMatches = getMatches(htmlString, regex);
+                        if (paragraphMatches.length > 0){
+                            for (var i = 0; i < paragraphMatches.length; i++){
+                                var translatedWord = dictionary[key].translation;
+                                var match = paragraphMatches[i];
+                                var tooltip = "";
+                                if (storage.tooltip){
+                                    tooltip = 'data-tippy="' + match[2] + '" data-tippy-delay="500" data-tippy-animation="perspective"';
                                 }
+                                var beginningElement = '<element ' + changeWordColor + ' id="translation' + dictionaryTranslationsCount + '" ' + tooltip + '>' + ' ';
+                                var endingElement = ' ' + '</element>';
+                                if (match[2].charAt(0) == key.charAt(0).toUpperCase()){
+                                    // word we are translating is uppercase, so change key to uppercase to match it
+                                    translatedWord = translatedWord.charAt(0).toUpperCase() + translatedWord.slice(1);
+                                }
+                                if (/[^a-z|A-Z]/i.test(match[1].substr(-1))){
+                                    // if there is something non-alphanumeric right before our keyword it's probably a hyphenated word or a punction of some type, so don't add a space
+                                    beginningElement = '<element ' + changeWordColor + ' id="translation' + dictionaryTranslationsCount + '" ' + tooltip + '>';
+                                }
+                                if (match[0].toLowerCase().includes(key+'s')){
+                                    // word is most likely plural so don't add space at the end to keep the original word's "s"
+                                    endingElement = "</element>";
+                                }
+                                var fullTextToSwap = ">" + match[1] + beginningElement + translatedWord + endingElement + match[3];
+                                htmlString = htmlString.replace(match[0], fullTextToSwap);
+                                dictionaryTranslationsCount += 1;
                             }
-                        } while (paragraphMatches.length > 0);
-                    }
-                });
-                paragraph.html(htmlString);
+                        }
+                    } while (paragraphMatches.length > 0);
+                }
             });
+            bodyElement.outerHTML = bodyHTML;
         }
     });
 }
