f67a39beef15914080db71a25435d0dc4e45b252
Michael Mroczka <michaelmroczka@gmail.com>
2018-12-08 11:55:57 -0500

    Finally got dictionary export working

--- my_test_extension/js/dictionary.js
+++ my_test_extension/js/dictionary.js
@@ -12,19 +12,21 @@ document.addEventListener('DOMContentLoaded', function () { // Settings
         window.open("html/popup.html", "Upload Your Dictionary", "height=500,width=500");
     });
 
-    // // Add Export Button action
-    // document.getElementById("export").addEventListener("click", function() {
-    //     chrome.storage.sync.get(null, function(storage) {
-    //         dictionary = JSON.parse(storage.dictionary);
-    //         var data = JSON.stringify(dictionary, null, 2);
-    //         // alert(data);
-    //         // var url = 'data:text/json;charset=utf8,' + encodeURIComponent(data);
-    //         // window.open(url, '_blank');
-    //         var FileSaver = require('file-saver');
-    //         var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
-    //         FileSaver.saveAs(blob, "dictionary.txt");
-    //     })
-    // });
+    // Add Export Button action
+    document.getElementById("export").addEventListener("click", function() {
+        chrome.storage.sync.get(null, function(storage) {
+            dictionary = JSON.parse(storage.dictionary);
+            var data = JSON.stringify(dictionary);
+            const filename = 'my_dictionary.txt';
+            let element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
+            element.setAttribute('download', filename);
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
+        })
+    });
 
     // Add Clear Button action
     document.getElementById("clear").addEventListener("click", function() {
