47d5f357861fbdea0b4678fb1dafcf752585053f
Michael Mroczka <michaelmroczka@gmail.com>
2018-12-08 13:08:28 -0500

    Change file structure hierarchy

--- helloworld.py
+++
@@ -1,7 +0,0 @@
-a = 10
-b = 30
-
-def addition():
-    document.getElementById("result").innerHTML = a+b
-
-document.getElementById("computebutton").addEventListener("click",addition())
--- make.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rapydscript -b -p -m helloworld.py > helloworld.js
